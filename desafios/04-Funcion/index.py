e2ea0be599e6d422a2ed4502b13c30b17ff74c05
JesusRamirezGamarra <luciojesusramirezgamarra@gmail.com>
2022-08-28 19:35:38 -0500

    desafio 04-Funcion

--- desafios/04-Funcion/index.py
+++ desafios/04-Funcion/index.py
@@ -1,9 +1,10 @@
 # El papa Gregorio XIII, asesorado por el astrónomo jesuita Christopher Clavius, el 24 de febrero de 1582 promulgó la bula Inter Gravissimas, en la que establecía que tras el jueves 4 de octubre de 1582 seguiría el viernes 15 de octubre de 1582.
 # Con la eliminación de estos diez días desapareció el desfase con el año solar. Para que no volviera a ocurrir, en el nuevo calendario se eliminaron tres años bisiestos cada cuatro siglos. Con lo anterior, el 4 de octubre de 1582 fue el último día del calendario juliano y el 15 de octubre de 1582 constituyó el primer día del calendario gregoriano. Por tal razón no existieron las fechas del 5 al 14 de octubre de dicho año.
 # Si se usan métodos actuales, el cálculo de fechas anteriores al 15 de octubre de 1582 siempre será erróneo, ya que se deben utilizar exclusivamente en retrospectiva hasta esta fecha y cambiar a cálculo de fechas julianas a partir del 4 de octubre de 1582, sin olvidar estos 10 días inexistentes.
+#import inspect
 
 rd={'type':int,'Year':'Only leap years','docstring':'Validate that the entered year is a leap year, > 1582'}
-def año_bisiesto(year)->rd:
+def año_bisiesto(year:'int')->rd:
     try:
         year = int(year)
         if year >1581 :
@@ -16,4 +17,7 @@ def año_bisiesto(year)->rd:
 
 if __name__ == '__main__':
     year = input(f"Ingrese un año :")
+    print(año_bisiesto.__annotations__)
+    #print(inspect.getfullargspec(año_bisiesto))
     año_bisiesto(year)
+    
